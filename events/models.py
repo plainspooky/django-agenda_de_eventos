e872166d3ea165c8511365e024fedb83e1994f1a
Giovanni Nunes <giovanni.nunes@gmail.com>
2018-04-02 00:01:36 -0300

    inclusão do modelo

--- events/models.py
+++ events/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+
+    priorities_list = (
+        ('0', 'Sem prioridade'),
+        ('1', 'Normal'),
+        ('2', 'Urgente'),
+        ('3', 'Muito Urgente'),
+    )
+
+    date = models.DateField()
+    event = models.CharField(max_length=80)
+    priority = models.CharField(max_length=1, choices=priorities_list)
+
+    def __str__(self):
+        return self.event
+

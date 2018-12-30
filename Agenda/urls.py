7279818b75fd7cc765ea739b751923e45f58d97f
Giovanni Nunes <giovanni.nunes@gmail.com>
2018-12-30 09:48:17 -0200

    Outros arquivos esquecidos na formatação via black

--- Agenda/urls.py
+++ Agenda/urls.py
@@ -16,8 +16,4 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('events.urls')),
-]
-
+urlpatterns = [path("admin/", admin.site.urls), path("", include("events.urls"))]
